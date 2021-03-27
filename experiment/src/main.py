be51263619f4264ad1a037cc95e7f8c8841dad94
bwqr <ruzgardeniz.08@hotmail.com>
2021-03-27 11:31:43 +0300

    add initial transmitter implementation

--- experiment/src/main.py
+++ experiment/src/main.py
@@ -1,15 +1,20 @@
 from enum import IntEnum
 
+start_delimiter = 'start_delimiter'
+end_delimiter = 'end_delimiter'
+
 
 class Spray(IntEnum):
-    SPRAY_1 = 0
-    SPRAY_2 = 1
+    Spray_1 = 0
+    Spray_2 = 1
+
+
+class Encoders(IntEnum):
+    ByteEncoder = 0
 
 
 class Encoder:
-    def id(self) -> str:
-        """Identifier of this encoder"""
-        pass
+    id: int
 
     def encode(self, state) -> str:
         """Encode given state into string"""
@@ -46,12 +51,11 @@ class State:
         print(self.encoder.encode(self))
 
 
-class ByteEncoder(Encoder):
-    def id(self) -> str:
-        return 'byte_encoder'
+class WordEncoder(Encoder):
+    id: int = int(Encoders.ByteEncoder)
 
     def encode(self, state: State) -> str:
-        output = self.id() + '\n'
+        output = start_delimiter + '\n' + str(WordEncoder.id) + '\n'
         for act in state.actions:
             output += self._encode_action(act) + '\n'
 
@@ -59,19 +63,20 @@ class ByteEncoder(Encoder):
 
     def _encode_action(self, act: Action) -> str:
         if isinstance(act, Emit):
-            return 'emitting the alcohol for {} duration from spray {}'.format(act.duration, act.spray)
+            return 'emit\n{}\n{}'.format(int(act.spray), act.duration)
         elif isinstance(act, Wait):
-            return 'waiting for {} duration'.format(act.duration)
+            return 'wait\n{}'.format(int(act.duration))
         else:
-            return 'unimplemented action {} for {} encoder'.format(act.__class__, self.__class__)
+            print('unimplemented action {} for {} encoder'.format(act.__class__, self.__class__))
+            return ''
 
 
 def run():
     spray_duration = 100  # ms
     pause_duration = 200  # ms
 
-    state = State(ByteEncoder())
-    state.emit(Spray.SPRAY_1, spray_duration)
+    state = State(WordEncoder())
+    state.emit(Spray.Spray_1, spray_duration)
     state.wait(pause_duration)
     state.execute()
 
---
+++ testbed/transmitter/main.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
+
+const char * start_delimiter = "start_delimiter";
+const char * end_delimiter = "end_delimiter";
+
+enum Spray {
+    Spray_1,
+    Spray_2
+};
+
+enum Decoders {
+    WordEncoder
+};
+
+struct Action {
+    virtual void act() const = 0;
+};
+
+struct Emit : Action {
+    Spray spray;
+    int duration;
+
+    void act() const override {
+        std::cout << "emitting the alcohol for " << this->duration << " duration from spray " << this->spray << std::endl;
+    }
+};
+
+struct Wait : Action {
+    int duration;
+
+    void act() const override {
+        std::cout << "waiting for " << this->duration <<" duration" << std::endl;
+    }
+};
+
+struct State {
+    std::vector<std::unique_ptr<Action>> actions;
+
+    void execute() {
+        for (const auto &action: this->actions) {
+            action->act();
+        }
+    }
+};
+
+struct Decoder {
+    static const int id;
+    virtual State decode() const = 0;
+};
+
+struct WordEncoder : Decoder {
+    static const int id = Decoders::WordEncoder;
+
+    State decode() const override {
+
+        State state;
+
+        std::cout << "decoding" << std::endl;
+        for (std::string line;std::getline(std::cin, line);) {
+            if (line == end_delimiter) {
+                break;
+            }
+
+            if (line == "emit") {
+                auto p = new Emit();
+
+                std::getline(std::cin, line);
+
+                p->spray = static_cast<Spray>(std::stoi(line));
+
+                std::getline(std::cin, line);
+
+                p->duration = std::stoi(line);
+
+                state.actions.push_back(std::unique_ptr<Action>(p));
+            } else if (line == "wait") {
+                auto p = new Wait();
+
+                std::getline(std::cin, line);
+
+                p->duration = std::stoi(line);
+            
+                state.actions.push_back(std::unique_ptr<Action>(p));
+            } else {
+                std::cout << "unknown action " << line << std::endl; 
+            }
+        }
+
+        std::cout << "decoded" << std::endl;
+
+        return state;
+    }
+};
+
+std::unique_ptr<Decoder> find_decoder(int decoder_id) {
+    if (decoder_id == WordEncoder::id) {
+        return std::unique_ptr<Decoder>(new struct WordEncoder());
+    }
+
+    return nullptr;
+}
+
+int main(int argc, char **argv) {
+    std::string line;
+
+    // consume until eof or start_delimiter
+    while(line != start_delimiter && std::getline(std::cin, line));
+
+    std::getline(std::cin, line);
+
+    int decoder_id = std::stoi(line.c_str());
+    std::unique_ptr<Decoder> decoder = find_decoder(decoder_id);
+
+    if (!decoder) {
+        std::cout << "unknown decoder " << decoder_id << std::endl;
+
+        return -1;
+    }
+
+    State state = decoder->decode();
+
+    state.execute();
+
+    return 0;
+}
